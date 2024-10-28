2fd7d96d3cd5ca3a15386b3196ffbf773fb30abf
johnyob <ajo41@cam.ac.uk>
2024-10-28 14:25:04 +0000

    chore(nix): add empty flake

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Type Inference Zoo Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # OCaml overlay
+    ocaml-overlay = {
+      url = "github:nix-ocaml/nix-overlays";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            ocaml-overlay.overlays.default
+            (import ./nix/overlay.nix)
+          ];
+        };
+
+        fmt = treefmt.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+
+          programs.alejandra.enable = true;
+          programs.ocamlformat = {
+            enable = true;
+            package = pkgs.ocamlformat_0_26_2;
+          };
+
+          settings.global.excludes = ["result" ".direnv" "_build"];
+        };
+      in {
+        formatter = fmt.config.build.wrapper;
+
+        devShells.default = pkgs.mkShell {
+          name = "tizoo-dev-shell";
+
+          buildInputs = with pkgs; [
+            # Formatters
+            alejandra
+            ocamlformat_0_26_2
+
+            # OCaml devenv
+            ocamlPackages.utop
+            ocamlPackages.ocaml-lsp
+            ocamlPackages.merlin
+            ocamlPackages.merlin-lib
+          ];
+        };
+      });
+}
---
+++ nix/overlay.nix
@@ -0,0 +1,4 @@
+final: prev:
+with prev; {
+  ocamlPackages = final.ocaml-ng.ocamlPackages_5_2;
+}

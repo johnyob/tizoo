022ef98022ef56ff55ad6cbc2d0ec1a43494fa68
johnyob <ajo41@cam.ac.uk>
2025-01-03 10:53:49 +0000

    feat(solver): implement solver for suspended match constraints

--- lib/constraint_solver/solver.ml
+++ lib/constraint_solver/solver.ml
@@ -28,6 +28,18 @@ module Env = struct
   let find_var t expr_var = Map.find_exn t.expr_vars expr_var
   let enter_region ~state t = { t with curr_region = G.enter_region ~state t.curr_region }
   let exit_region ~state:_ t root = G.exit_region ~curr_region:t.curr_region root
+
+  let of_gclosure
+    (gclosure : G.Suspended_match.closure)
+    ~(closure : C.Closure.t)
+    ~curr_region
+    =
+    let type_vars =
+      List.zip_exn (Set.to_list closure.type_vars) gclosure.variables
+      |> C.Type.Var.Map.of_alist_exn
+    in
+    { (empty curr_region) with type_vars }
+  ;;
 end
 
 let rec gtype_of_type : state:State.t -> env:Env.t -> C.Type.t -> G.Type.t =
@@ -44,6 +56,33 @@ let rec gtype_of_type : state:State.t -> env:Env.t -> C.Type.t -> G.Type.t =
       ~state
       ~curr_region:env.curr_region
       (Constr (List.map args ~f:self, constr))
+;;
+
+let match_type
+  : state:State.t -> env:Env.t -> G.Type.t Structure.Former.t -> Env.t * C.Type.Matchee.t
+  =
+  fun ~state ~env former ->
+  let match_types ~env gtypes =
+    List.fold_map gtypes ~init:env ~f:(fun env gtype ->
+      let type_var = C.Type.Var.create ~id_source:state.id_source () in
+      Env.bind_type_var env ~var:type_var ~type_:gtype, type_var)
+  in
+  match former with
+  | Tuple gtypes ->
+    let env, type_vars= match_types ~env gtypes in
+    env, Tuple type_vars
+  | Arrow (gtype1, gtype2) ->
+    let type_var1 = C.Type.Var.create ~id_source:state.id_source () in
+    let type_var2 = C.Type.Var.create ~id_source:state.id_source () in
+    let env =
+      env
+      |> Env.bind_type_var ~var:type_var1 ~type_:gtype1
+      |> Env.bind_type_var ~var:type_var2 ~type_:gtype2
+    in
+    env, Arrow (type_var1, type_var2)
+  | Constr (gtypes, constr) ->
+    let env, type_vars = match_types ~env gtypes in
+    env, Constr (type_vars, constr)
 ;;
 
 let exists ~(state : State.t) ~env ~type_var =
@@ -105,6 +144,36 @@ let rec solve : state:State.t -> env:Env.t -> C.t -> unit =
     [%log.global.debug "Updated env" (env : Env.t)];
     [%log.global.debug "Solving exist body"];
     self ~state ~env cst
+  | Match (matchee, closure, f) ->
+    let matchee = Env.find_type_var env matchee in
+    [%log.global.debug "Matchee type" (matchee : Type.t)];
+    let gclosure = gclosure_of_closure ~env closure in
+    [%log.global.debug
+      "Closure of suspended match" (gclosure : G.Suspended_match.closure)];
+    let case ~curr_region structure =
+      [%log.global.debug "Entered match handler" (structure : Type.t Structure.Former.t)];
+      (* Enter region and construct env *)
+      let env = Env.of_gclosure gclosure ~closure ~curr_region in
+      [%log.global.debug "Handler env" (env : Env.t)];
+      [%log.global.debug "Handler state" (state : State.t)];
+      (* Solve *)
+      let env, matchee = match_type ~state ~env structure in
+      [%log.global.debug
+        "Matchee and updated env" (matchee : C.Type.Matchee.t) (env : Env.t)];
+      let cst = f matchee in
+      [%log.global.debug "Generated constraint from case" (cst : C.t)];
+      solve ~state ~env cst;
+      [%log.global.debug "Solved generated constraint" (cst : C.t)];
+      [%log.global.debug "Exiting case region"]
+    in
+    [%log.global.debug "Suspending match..."];
+    G.suspend ~state ~curr_region:env.curr_region { matchee; closure = gclosure; case }
+
+and gclosure_of_closure ~env closure : G.Suspended_match.closure =
+  let variables =
+    closure.type_vars |> Set.to_list |> List.map ~f:(Env.find_type_var env)
+  in
+  { variables }
 
 and gscheme_of_scheme ~state ~env { type_vars; in_; type_ } =
   let env = Env.enter_region ~state env in
@@ -134,6 +203,13 @@ let solve : C.t -> unit Or_error.t =
     G.force_generalization ~state env.curr_region;
     [%log.global.debug "Generalized root region" (env.curr_region : Type.region_node)];
     [%log.global.debug "End state" (state : State.t)];
+    let num_zombie_regions =
+      G.Generalization_tree.num_zombie_regions state.generalization_tree
+    in
+    if num_zombie_regions > 0
+    then (
+      [%log.global.error "num_zombie_regions" (num_zombie_regions : int)];
+      raise G.Cannot_unsuspend_generic);
     Ok ()
   with
   | exn -> Or_error.error_s [%message "Failed to solve constraint" (exn : exn)]
--- lib/constraint_solver/structure.ml
+++ lib/constraint_solver/structure.ml
@@ -55,40 +55,89 @@ module Former = struct
   ;;
 end
 
-module First_order (S : S) = struct
+module Suspended_first_order (S : S) = struct
+  module Var = struct
+    type 'a t =
+      | Empty
+      | Empty_one_or_more_handlers of 'a handler list
+    [@@deriving sexp_of]
+
+    and 'a handler = { run : 'a S.t -> unit } [@@unboxed] [@@deriving sexp_of]
+
+    let merge t1 t2 =
+      match t1, t2 with
+      | Empty, Empty -> t1
+      | Empty, Empty_one_or_more_handlers _ -> t2
+      | Empty_one_or_more_handlers _, Empty -> t1
+      | Empty_one_or_more_handlers handlers1, Empty_one_or_more_handlers handlers2 ->
+        Empty_one_or_more_handlers (handlers1 @ handlers2)
+    ;;
+
+    let add_handler t handler =
+      match t with
+      | Empty -> Empty_one_or_more_handlers [ handler ]
+      | Empty_one_or_more_handlers handlers ->
+        Empty_one_or_more_handlers (handler :: handlers)
+    ;;
+
+    let fill t s ~schedule_handler =
+      match t with
+      | Empty -> ()
+      | Empty_one_or_more_handlers handlers -> List.iter handlers ~f:(schedule_handler s)
+    ;;
+  end
+
   type 'a t =
-    | Var
+    | Var of 'a Var.t
     | Structure of 'a S.t
   [@@deriving sexp_of]
 
   exception Cannot_merge = S.Cannot_merge
 
-  type 'a ctx = 'a S.ctx
+  type 'a ctx =
+    { schedule_handler : 'a S.t -> 'a Var.handler -> unit
+    ; super : 'a S.ctx
+    }
 
   let iter t ~f =
     match t with
-    | Var -> ()
+    | Var _ -> ()
     | Structure s -> S.iter s ~f
   ;;
 
   let fold t ~f ~init =
     match t with
-    | Var -> init
+    | Var _ -> init
     | Structure s -> S.fold s ~f ~init
   ;;
 
   let map t ~f =
     match t with
-    | Var -> Var
+    | Var var ->
+      (* FIXME(): [map] doesn't have the signature ['a t -> f:('a -> 'b) -> 'b t]
+
+         Because the ['a] in [Var] is in a negative position, making it
+         costly to map over. Additionally all of our uses of [map] in
+         [Generalization] are homomorphic. *)
+      Var var
     | Structure s -> Structure (S.map s ~f)
   ;;
 
   let merge ~ctx ~create ~unify ~type1 ~type2 t1 t2 =
     match t1, t2 with
-    | Var, Var -> Var
-    | Structure s, Var | Var, Structure s -> Structure s
+    | Var var1, Var var2 -> Var (Var.merge var1 var2)
+    | Structure s, Var var | Var var, Structure s ->
+      Var.fill var s ~schedule_handler:ctx.schedule_handler;
+      Structure s
     | Structure s1, Structure s2 ->
       Structure
-        (S.merge ~ctx ~create:(fun s -> create (Structure s)) ~unify ~type1 ~type2 s1 s2)
+        (S.merge
+           ~ctx:ctx.super
+           ~create:(fun s -> create (Structure s))
+           ~unify
+           ~type1
+           ~type2
+           s1
+           s2)
   ;;
 end

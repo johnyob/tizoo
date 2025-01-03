0819e20ea228cf214f1f600f4ec18189fbf3ff80
johnyob <ajo41@cam.ac.uk>
2025-01-03 21:39:28 +0000

    feat: split CLI code into executable and `mlsus_main` library

---
+++ lib/main/mlsus_main.ml
@@ -0,0 +1,48 @@
+open Core
+open Mlsus_ast
+open Mlsus_parser
+open Mlsus_constraint
+
+let pp_structure ppf structure =
+  Fmt.pf ppf "@[%a@]" Sexp.pp_hum ([%sexp_of: Ast.structure] structure)
+;;
+
+let lex_and_print lexbuf =
+  match Lexer.read_tokens lexbuf with
+  | Ok tokens -> Fmt.(pr "@[<v>%a@]@." (list Token.pp)) tokens
+  | Error err -> Fmt.pr "@[%a@]@." Lexer.Error.pp err
+;;
+
+let parse lexbuf k =
+  match Parser.parse_structure lexbuf with
+  | Ok structure -> k structure
+  | Error err -> Fmt.pr "@[%a@]@." Parser.Error.pp err
+;;
+
+let parse_and_print lexbuf =
+  parse lexbuf @@ fun structure -> Fmt.pr "%a@." pp_structure structure
+;;
+
+let constraint_gen lexbuf ~dump_ast k =
+  parse lexbuf
+  @@ fun structure ->
+  if dump_ast then Fmt.pr "Parsed structure:@.%a.@." pp_structure structure;
+  match Mlsus_type_checker.infer_str structure with
+  | Ok cst -> k cst
+  | Error err -> Fmt.pr "@[%a@]@." Error.pp err
+;;
+
+let pp_constraint ppf cst = Fmt.pf ppf "@[%a@]" Sexp.pp_hum ([%sexp_of: Constraint.t] cst)
+
+let constraint_gen_and_print lexbuf ~dump_ast =
+  constraint_gen lexbuf ~dump_ast @@ fun cst -> Fmt.pr "%a@." pp_constraint cst
+;;
+
+let type_check_and_print lexbuf ~dump_ast ~dump_constraint =
+  constraint_gen lexbuf ~dump_ast
+  @@ fun cst ->
+  if dump_constraint then Fmt.pr "Generated constraint:@.%a@." pp_constraint cst;
+  match Mlsus_constraint_solver.solve cst with
+  | Ok () -> Fmt.pr "Well typed :)@."
+  | Error err -> Fmt.pr "@[%a@]@." Error.pp err
+;;

f8fa2654c5fa02e759ccd9b149f68f59468100c1
johnyob <ajo41@cam.ac.uk>
2025-01-07 21:22:31 +0000

    refactor(main): update API to pass optional source to parsers

--- lib/main/mlsus_main.mli
+++ lib/main/mlsus_main.mli
@@ -1,17 +1,19 @@
+open Grace
+
 (** [lex_and_print lexbuf] reads the tokens in [lexbuf] and prints them.
     Any errors are additionally printed. *)
 val lex_and_print : Lexing.lexbuf -> unit
 
 (** [parse_and_print lexbuf] parses the [lexbuf] as a structure and prints it.
     Any errors are additionally printed. *)
-val parse_and_print : Lexing.lexbuf -> unit
+val parse_and_print : ?source:Source.t -> Lexing.lexbuf -> unit
 
 (** [constraint_gen_and_print lexbuf ~dump_ast] parses the [lexbuf] as a structure,
     generates a well-typedness constraint, and then prints it.
 
     If [dump_ast] is [true], then the parsed structure is printed.
     Any errors are additionally printed. *)
-val constraint_gen_and_print : Lexing.lexbuf -> dump_ast:bool -> unit
+val constraint_gen_and_print : ?source:Source.t -> Lexing.lexbuf -> dump_ast:bool -> unit
 
 (** [type_check_and_print lexbuf ~dump_ast ~dump_constraint] parses the [lexbuf] as a structure,
     generates a well-typedness constraint, solves the constraint and prints the result.
@@ -19,4 +21,9 @@ val constraint_gen_and_print : Lexing.lexbuf -> dump_ast:bool -> unit
     If [dump_ast] is [true], then the parsed structure is printed.
     If [dump_constraint] is [true], then the generated constraint is printed.
     Any errors are additionally printed. *)
-val type_check_and_print : Lexing.lexbuf -> dump_ast:bool -> dump_constraint:bool -> unit
+val type_check_and_print
+  :  ?source:Source.t
+  -> Lexing.lexbuf
+  -> dump_ast:bool
+  -> dump_constraint:bool
+  -> unit
